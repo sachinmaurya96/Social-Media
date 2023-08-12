76fa6cf342da8191ef4513ba15a88f155cfa76c5
sachinmaurya96 <125910801+sachinmaurya96@users.noreply.github.com>
2023-08-12 12:08:33 +0530

    first commit of social media app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <div className="blur"></div>
+      <div className="blur"></div>
+    </div>
+  );
+}
+
+export default App;
